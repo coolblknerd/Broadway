6f960d5f9db4b704779abfeabb83a760a43e48c1
Reggie Davis <reginald.davis88@gmail.com>
2016-06-04 16:06:31 -0500

    Forms CRUD & Views for Plays

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,4 +1,5 @@
 class PlaysController < ApplicationController
+  before_action :find_play, only: [:show, :edit, :update, :destroy]
 
   def index
     @plays = Play.all.order("created_at DESC")
@@ -8,23 +9,43 @@ class PlaysController < ApplicationController
   end
 
   def new
+    @play = Play.new
   end
 
   def create
+    @play = Play.create(play_params)
+
+    if @play.save
+      redirect_to @play
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
+  def update
+    if @play.update(play_params)
+      redirect_to @play
+    else
+      render 'edit'
+    end
+  end
+
   def destroy
+    @play.destroy
+    redirect_to root_path
   end
 
   private
 
   def find_play
+    @play = Play.find(params[:id])
   end
 
   def play_params
+    params.require(:play).permit(:title, :description, :director)
   end
 
 end
