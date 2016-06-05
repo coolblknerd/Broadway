7336b3497bc1818927e847b793a1ab8a34bb171d
Reggie Davis <reginald.davis88@gmail.com>
2016-06-05 11:38:42 -0500

    Added Category Dropdown

--- app/models/play.rb
+++ app/models/play.rb
@@ -3,7 +3,7 @@ class Play < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
 
-  # validates :title, presence: true
-  # validates :description, presence: true
-  # validates :director, presence: true, length: { in: 1..3 }
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :director, presence: true
 end
