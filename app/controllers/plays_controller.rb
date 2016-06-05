3040382e1409e7be98d2e499d43e8d39b2a27556
Reggie Davis <reginald.davis88@gmail.com>
2016-06-05 09:49:51 -0500

    Category Model & User/Play Assoc.

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -9,11 +9,11 @@ class PlaysController < ApplicationController
   end
 
   def new
-    @play = Play.new
+    @play = current_user.play.build
   end
 
   def create
-    @play = Play.create(play_params)
+    @play = current_user.play.build(play_params)
 
     if @play.save
       redirect_to @play
--- app/models/play.rb
+++ app/models/play.rb
@@ -1,2 +1,9 @@
 class Play < ActiveRecord::Base
+
+  belongs_to :user
+  belongs_to :category
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :director, presence: true, length: { in: 1..3 }
 end
