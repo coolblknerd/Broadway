408d1cd212466656534804c23376ef180c46ff6a
Reggie Davis <reginald.davis88@gmail.com>
2016-06-05 12:15:07 -0500

    Added Paperclip & Changed form files

--- app/models/play.rb
+++ app/models/play.rb
@@ -6,4 +6,7 @@ class Play < ActiveRecord::Base
   validates :title, presence: true
   validates :description, presence: true
   validates :director, presence: true
+
+  has_attached_file :play_img, styles: { play_index: "250x350>", play_show: "325x475>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :play_img, content_type: /\Aimage\/.*\Z/
 end
